07480bee939a53ba96a1856a809d4f822d6abe74
timotiousPrime <timodb031@gmail.com>
2021-08-29 00:54:24 +0200

    made changes

---
+++ stateMutators.js
@@ -0,0 +1,18 @@
+
+// TODO: update players turn
+// TODO: log cells played by each user
+// TODO: 
+// TODO: 
+// TODO: 
+// TODO: 
+// TODO: 
+
+
+
+function UpdatePlayersTurn(){
+
+}
+
+function logCellPlayed(cell){
+    
+}
