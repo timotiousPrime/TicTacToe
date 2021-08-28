68a2771ca4643fe7acd8c76f61f53c0e7b8059e1
timotiousPrime <timodb031@gmail.com>
2021-08-29 00:54:05 +0200

    made changes

--- main.js
+++ main.js
@@ -1,18 +1,14 @@
-function createPlayer(name, token, canPlay, isWinner) {
-    let winCount = 0
-    return {
-        name,
-        token,
-        canPlay,
-        winCount: (isWinner) => {
-            isWinner ? ++winCount : winCount
-            return winCount
-        }
-    }
-}
+import { createPlayer } from './player.js'
+import { logCell } from './eventhandlers.js'
+
+let PlayerJim = createPlayer('jim', null, true, null, true)
+
+console.log(PlayerJim)
 
 const gameBoard = {
-    cellsUsed: {},
-    cellsAvailable: {},
+    cellsUsed: [],
+    cellsAvailable: [1,2,3,4,5,6,7,8,9] 
 }
 
+logCell(gameBoard)
+console.table (gameBoard)
