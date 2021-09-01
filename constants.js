47970f0ecbc57e08314b48bf986a66b56d75c1a9
timotiousPrime <timodb031@gmail.com>
2021-09-01 22:07:00 +0200

    file for all constants

---
+++ constants.js
@@ -0,0 +1,12 @@
+export const EL_IDS = {
+    GAMEBOARD: 'board',
+    CELL1: 'cell1',
+    CELL2: 'cell2',
+    CELL3: 'cell3',
+    CELL4: 'cell4',
+    CELL5: 'cell5',
+    CELL6: 'cell6',
+    CELL7: 'cell7',
+    CELL8: 'cell8',
+    CELL9: 'cell9',
+}
