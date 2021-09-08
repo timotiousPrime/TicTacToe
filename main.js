6fe79a83d50a90be69f9d5978d7f5ce85fd2005a
timotiousPrime <timodb031@gmail.com>
2021-09-08 22:10:58 +0200

    cleaned up code

--- main.js
+++ main.js
@@ -1,28 +1,13 @@
-import { board } from "./gameBoard.js"
+import { gameBoard } from "./gameBoard.js"
 import { getCellClicked } from "./eventhandlers.js"
 
 
 const player = (isHuman, token) => {
     let totalWins = 0
     let canPlay = false
-    return { isHuman, token, totalWins, canPlay }
+    let cellsUsed = []
+    return { isHuman, token, totalWins, canPlay, cellsUsed }
 }
 
-// const board = ( () => {
-//     let availableCells = [1,2,3,4,5,6,7,8,9]
-//     let cellsUsed = []
-//     let playerOneCells = []
-//     let playerTwoCells = []
-//     const winningCells = [
-//         [1,5,9],
-//         [3,5,7],
-//         [1,2,3],
-//         [4,5,6],
-//         [7,8,9],
-//         [1,4,7],
-//         [2,5,8],
-//         [3,6,9],
-//     ]
-
-// })()
 getCellClicked()
+
