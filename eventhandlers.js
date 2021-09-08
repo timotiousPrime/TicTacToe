dde9f87002ecfe94342463c202398ae8a072d272
timotiousPrime <timodb031@gmail.com>
2021-09-08 22:09:48 +0200

    handles cell clicks & updates board cells used/ava

--- eventhandlers.js
+++ eventhandlers.js
@@ -1,12 +1,25 @@
 import { EL_IDS } from './constants.js'
+import { gameBoard } from './gameBoard.js'
 
 export function getCellClicked(){
     let BOARD_CELLS = document.querySelectorAll('.boardCell')
 
-    BOARD_CELLS = Array.from(BOARD_CELLS)
+    // BOARD_CELLS = Array.from(BOARD_CELLS)
 
     BOARD_CELLS.forEach( cell => { 
-        cell.addEventListener('click', console.log('you clicked something'))
+        cell.addEventListener('click', handleClickedCell)
+        // console.log('testing')
     })
 
+}
+
+function handleClickedCell(e) {
+    const cellKey = Number(e.target.dataset.key)
+    gameBoard.availableCells.includes(cellKey) ? updateBoard(cellKey) : console.log('no can do')
+}
+
+function updateBoard(cellKey) {
+    const availCellPos = gameBoard.availableCells.indexOf(cellKey)
+    gameBoard.cellsUsed.push(cellKey)
+    gameBoard.availableCells.splice(availCellPos,1)
 }
