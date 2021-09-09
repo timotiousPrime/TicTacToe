ff65fa05791a1bf4a2ec0edeab54194b9592b9ee
Timo <timodb031@gmail.com>
2021-09-09 16:08:10 +0200

    WIP - Gets the player selected and clicked cell

--- eventhandlers.js
+++ eventhandlers.js
@@ -1,25 +1,29 @@
 import { EL_IDS } from './constants.js'
 import { gameBoard } from './gameBoard.js'
-
+import { GAME_STATE } from './state.js'
+ 
 export function getCellClicked(){
     let BOARD_CELLS = document.querySelectorAll('.boardCell')
 
-    // BOARD_CELLS = Array.from(BOARD_CELLS)
-
     BOARD_CELLS.forEach( cell => { 
         cell.addEventListener('click', handleClickedCell)
-        // console.log('testing')
     })
-
 }
 
 function handleClickedCell(e) {
     const cellKey = Number(e.target.dataset.key)
-    gameBoard.availableCells.includes(cellKey) ? updateBoard(cellKey) : console.log('no can do')
+    gameBoard.availableCells.includes(cellKey) ? updateBoard(cellKey, GAME_STATE.playersTurn) :
+    console.log('no can do')
 }
 
-function updateBoard(cellKey) {
+function updateBoard(cellKey, player) {
     const availCellPos = gameBoard.availableCells.indexOf(cellKey)
     gameBoard.cellsUsed.push(cellKey)
+    player.cellsUsed.push(cellKey)
     gameBoard.availableCells.splice(availCellPos,1)
+}
+
+export function getPlayerOne () {
+    const playerOneSelect = document.getElementById('playerOneSelect')
+    console.log(playerOneSelect.value)
 }
