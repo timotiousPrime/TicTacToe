96329ac98f04a98a95b9d71fd3de5dcd2efa33f0
timotiousPrime <timodb031@gmail.com>
2021-09-09 22:25:14 +0200

    cleaned up main

--- main.js
+++ main.js
@@ -1,14 +1,5 @@
-import { gameBoard } from "./gameBoard.js"
-import { getCellClicked, getPlayerOne } from "./eventhandlers.js"
+// import { gameBoard } from "./gameBoard.js"
+import { handleGameCells } from "./eventhandlers.js"
 
 
-const player = (isHuman, token) => {
-    let totalWins = 0
-    let canPlay = false
-    let cellsUsed = []
-    return { isHuman, token, totalWins, canPlay, cellsUsed }
-}
-
-getCellClicked()
-
-getPlayerOne()
+handleGameCells()
