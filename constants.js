f358ec5bb1046513ad014892359af10618dedea2
Timo <timodb031@gmail.com>
2021-09-10 08:38:34 +0200

    moved winningCells from gameBoard to constants

--- constants.js
+++ constants.js
@@ -14,6 +14,17 @@ export const EL_IDS = {
     CELL9: 'cell9',
 }
 
+export const winningCells = [
+    [1,5,9],
+    [3,5,7],
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [1,4,7],
+    [2,5,8],
+    [3,6,9],
+]
+
 export const GAME_MODE = {
     INIT: 'init',
     RUNNING: 'running',
