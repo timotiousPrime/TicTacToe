e15bd36b670ca88a94cef42fc340dd667faedd28
timotiousPrime <timodb031@gmail.com>
2021-09-16 21:49:15 +0200

    display player tokens on board

--- eventhandlers.js
+++ eventhandlers.js
@@ -1,4 +1,4 @@
-import { EL_IDS, playerOne, playerTwo, winningCells } from './constants.js'
+import { EL_IDS, playerOne, playerTwo, WINNING_CELLS } from './constants.js'
 import { gameBoard } from './gameBoard.js'
 import { GAME_STATE } from './constants.js'
  
@@ -6,14 +6,15 @@ export function handleGameCells(){
     const BOARD_CELLS = document.querySelectorAll('.boardCell')
 
     BOARD_CELLS.forEach( cell => { 
-        cell.addEventListener('click', handleClickedCell)
+        cell.addEventListener('click', handleClickedCell, )
     })
 }
 
 function handleClickedCell(e) {
-    const cellKey = Number(e.target.dataset.key)
+    const cell = e.target
+    const cellKey = Number(cell.dataset.key)
     if (gameBoard.availableCells.includes(cellKey)) {
-        updateState(cellKey, GAME_STATE)
+        updateState(cell, GAME_STATE)
     }
 }
 
@@ -24,24 +25,45 @@ function updateState(cell, state) {
     console.log(state)
 }
 
-// Checks the cells from the winning cells array are in the players used array
-// returns true if true
-function checkCells(cell) {
-    return GAME_STATE.playersTurn.cellsUsed.includes(cell)
+function updateBoard(cell, state) {
+    const cellKey = Number(cell.dataset.key)
+    const cellID =  cell.id
+    const availCellPos = gameBoard.availableCells.indexOf(cellKey)
+    gameBoard.cellsUsed.push(cellKey)
+    state.playersTurn.cellsUsed.push(cellKey)
+    gameBoard.availableCells.splice(availCellPos,1)
+    displayToken(cellID, GAME_STATE)
+}
+
+function displayToken(cellID, state) {
+    const cell = document.getElementById(cellID)
+    console.log(state.playersTurn.token)
+    console.log(state)
+    cell.classList.add(state.playersTurn.token)
 }
 
 function checkForWin(state) {
-    winningCells.forEach( cellsArray => {
+    WINNING_CELLS.forEach( cellsArray => {
         if (cellsArray.every(checkCells)) {
             declareWinner(state)}
     })    
 }
 
-function updateBoard(cellKey, state) {
-    const availCellPos = gameBoard.availableCells.indexOf(cellKey)
-    gameBoard.cellsUsed.push(cellKey)
-    state.playersTurn.cellsUsed.push(cellKey)
-    gameBoard.availableCells.splice(availCellPos,1)
+// Checks the cells from the winning cells array are in the players used array
+// returns true if true
+function checkCells(cell) {
+    return GAME_STATE.playersTurn.cellsUsed.includes(cell)
+}    
+
+// Think about making this function return a winner
+function declareWinner(state) {
+    countPlayersWin(state)
+    console.log(`${state.playersTurn.playerName} is the winner`)
+}
+
+function countPlayersWin(state) {
+    const player = state.playersTurn
+    ++player.totalWins
 }
 
 function updatePlayerTurn(state) {
@@ -50,14 +72,4 @@ function updatePlayerTurn(state) {
         state.playersTurn = playerOne
 }
 
-// Think about making this function return a winner
-function declareWinner(state) {
-    countPlayersWin(state)
-    console.log(`${player.playerName} is the winner`)
-}
-
-function countPlayersWin(state) {
-    const player = state.playersTurn
-    ++player.totalWins
-}
 
