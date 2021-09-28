90ff1dfb327c2edcdc212b93fdc73809ef20fa20
Timo <timodb031@gmail.com>
2021-09-28 16:14:06 +0200

    EasyBot mostly functional

--- easyBot.js
+++ easyBot.js
@@ -5,12 +5,16 @@ export function getRandomRemainingCellIndex(state) {
     return Math.floor(Math.random() * range -1 )
 }
 
-export function setAiChoice(state){
+export function getAiChoice(state){
     const randomCellIndex = getRandomRemainingCellIndex(state)
-    const cell = state.availableCells[randomCellIndex]
-    const cellById = document.getElementById(`cell${cell}`)
+    const cellValue = state.availableCells[randomCellIndex]
+    const cell = document.getElementById(`cell${cellValue}`)
     
-    cellById.classList.add(state.playersTurn.token)
+    // cellById.classList.add(state.playersTurn.token)
     console.log('choice has been made')
+    console.log(randomCellIndex)
+    console.log(cellValue)
+    console.log(cell)
+    return cell
 }
 
