d6d0a69f88716d1b21f8a6acb76182b7f161d731
Timo <timodb031@gmail.com>
2021-09-28 09:26:55 +0200

    logic for AI random cell choice

--- easyBot.js
+++ easyBot.js
@@ -1,7 +1,16 @@
 // A completely Random player
 
-function getRandomRemainingCell(state) {
+export function getRandomRemainingCellIndex(state) {
     const range = state.availableCells.length
-    return Math.floor(Math.random() * range +1)
+    return Math.floor(Math.random() * range -1 )
+}
+
+export function setAiChoice(state){
+    const randomCellIndex = getRandomRemainingCellIndex(state)
+    const cell = state.availableCells[randomCellIndex]
+    const cellById = document.getElementById(`cell${cell}`)
+    
+    cellById.classList.add(state.playersTurn.token)
+    console.log('choice has been made')
 }
 
