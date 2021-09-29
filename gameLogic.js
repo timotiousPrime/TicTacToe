f97ad3905b30444b4a310b20ecf3e531acccc992
timotiousPrime <timodb031@gmail.com>
2021-09-29 20:51:42 +0200

    checks if player has a winning combo

--- gameLogic.js
+++ gameLogic.js
@@ -38,3 +38,9 @@ function declareWinner(state) {
     countPlayersWin(player)
     console.log(`${state.playersTurn.playerName} is the winner`)
 }
+
+// Checks the cells from the winning cells array are in the players used array
+// returns true if true
+function checkCurrentPlayersCellsUsed(cell) {
+    return GAME_STATE.playersTurn.cellsUsed.includes(cell)
+}
