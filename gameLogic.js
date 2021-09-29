d9667d820dd08ef99f6eb3a641e1693374118cd7
timotiousPrime <timodb031@gmail.com>
2021-09-29 20:48:03 +0200

    checks win or draw in one function

--- gameLogic.js
+++ gameLogic.js
@@ -16,3 +16,18 @@ function getPlayersMove(state){
         getPlayersMove(state.playersTurn) :
         getAiMove(state.playersTurn)
 }
+
+function checkResult(state) {
+    // Check for win
+    WINNING_CELLS.forEach( cellsArray => {
+        if (cellsArray.every(checkCurrentPlayersCellsUsed)) {
+            declareWinner(state)
+            endGame(state, 'gameWin')
+        }
+    })    
+    // Check for draw
+    if (state.availableCells.length < 1 && state.gameMode !== 'game-win') {
+        endGame(state, 'gameDraw')
+        console.log('game is a draw')
+    }
+}
