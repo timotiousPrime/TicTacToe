6cd511dddfa6019e5689310f68a643795673f9f4
timotiousPrime <timodb031@gmail.com>
2021-10-02 23:11:27 +0200

    made a factory function and methods for board

--- constants.js
+++ constants.js
@@ -1,15 +1,14 @@
 // import { getPlayerOne } from "./eventhandlers.js"
-import { gameBoard } from "./gameBoard.js"
 
 export const WINNING_CELLS = [
-    [1,5,9],
-    [3,5,7],
-    [1,2,3],
-    [4,5,6],
-    [7,8,9],
-    [1,4,7],
-    [2,5,8],
-    [3,6,9],
+    [ 1, 5, 9 ] ,
+    [ 3, 5, 7 ],
+    [ 1, 2, 3 ],
+    [ 4, 5, 6 ],
+    [ 7, 8, 9 ],
+    [ 1, 4, 7 ],
+    [ 2, 5, 8 ],
+    [ 3, 6, 9 ],
 ]
 
 export const EL_IDS = {
@@ -23,34 +22,122 @@ export const EL_IDS = {
 }
 
 export const GAME_MODE = {
-    INIT: 'init',
     RUNNING: 'running',
-    GAME_WIN: 'game-win',
+    PLAYER_ONE_WIN: 'player-one-win',
+    PLAYER_TWO_WIN: 'player-two-win',
     GAME_DRAW: 'game-draw',
 }
 
 const player = (isHuman, token, playerName) => {
     let totalWins = 0
     let cellsUsed = []
-    return { isHuman, token, totalWins, cellsUsed, playerName }
+    let isWinner = false
+    return { isHuman, token, isWinner, totalWins, cellsUsed, playerName }
 }
 
-export const playerOne = player(false, 'x', 'playerOne')
+export const playerOne = player(true, 'x', 'playerOne')
 // Temporary until I decide how to create the second player
-export const playerTwo = player(false, 'o', 'playerTwo')
+export const playerTwo = player(true, 'o', 'playerTwo')
 
-export const INIT_STATE = {
-    gameMode: GAME_MODE.INIT,
-    players: [playerOne, playerTwo],
-    playersTurn: playerOne,
-    availableCells: [1,2,3,4,5,6,7,8,9],
-    cellsUsed: [],
+export function CreateNewBoard() {
+    let NewBoard = Object.create(BoardMethods)
+    NewBoard.availableCells = [1,2,3,4,5,6,7,8,9]
+    NewBoard.cellsUsed = []
+    NewBoard.players = [playerOne, playerTwo]
+    NewBoard.playersTurnAtBoard = playerOne
+    NewBoard.gameMode = GAME_MODE.RUNNING
+        
+    return NewBoard
 }
 
-export const GAME_STATE = {
-    gameMode: GAME_MODE.RUNNING,
-    players: [playerOne, playerTwo],
-    playersTurn: playerOne,
-    availableCells: [1,2,3,4,5,6,7,8,9],
-    cellsUsed: [],
+const BoardMethods = {
+
+    updatePlayersTurnAtBoard() {
+        return (this.playersTurnAtBoard.playerName === 'playerOne' ?
+        this.playersTurnAtBoard = playerTwo :
+        this.playersTurnAtBoard = playerOne)
+    },
+
+    updateBoardCells (cellUsed) {
+        const cellAvaibleIndex = this.availableCells.indexOf(cellUsed)
+        if (this.availableCells[cellAvaibleIndex] === cellUsed) {
+            const cellID = `cell${cellUsed}`
+            const boardCell = document.getElementById(cellID)
+            boardCell.classList.add(this.playersTurnAtBoard.token)
+            return (
+                this.availableCells.splice(cellAvaibleIndex,1),
+                this.cellsUsed.push(cellUsed)
+            )
+        }
+    },
+
+    updatePlayersUsedCells(cellUsed) {
+        return (this.playersTurnAtBoard.cellsUsed.push(cellUsed))
+    },
+
+    updateGameMode(result){
+        return (this.gameMode = result)
+    },
+    
+    checkResult(){
+        console.log(this)
+        // check for win
+        WINNING_CELLS.forEach( cellsArray => {
+            if (this.playersTurnAtBoard.cellsUsed.toString() === cellsArray.toString()) {
+                
+                console.log(this)
+                if (this.playersTurnAtBoard === playerOne) {
+                    this.updateWinResult(playerOne)
+                    this.endGame()
+                }
+                else {
+                    this.updateWinResult(playerTwo)
+                    this.endGame()
+                }
+            }
+        })
+        // Check for draw
+        if (this.availableCells.length < 1 && this.playersTurnAtBoard.isWinner == false) {
+            this.gameMode = GAME_MODE.GAME_DRAW
+            console.log('game is a draw')
+            this.endGame()
+        }
+    },
+
+    updateWinResult(player) {
+        if (player === playerOne) {
+            console.log('player one is the winner')
+            return (
+            this.gameMode = GAME_MODE.PLAYER_ONE_WIN,
+            this.playersTurnAtBoard.isWinner = true),
+            ++this.playersTurnAtBoard.totalWins
+        } else {
+            console.log('player two is the winner')
+
+            return(
+            this.gameMode = GAME_MODE.PLAYER_TWO_WIN,
+            this.playersTurnAtBoard.isWinner = true)
+        }
+    },
+
+    endGame(){
+        const player = this.playersTurnAtBoard
+        if (player.isWinner){
+            this.hideBoard ()
+            this.displayGameOverText(player)
+        } 
+    },
+
+    hideBoard() {
+        EL_IDS.overlay.classList.remove('invisible')
+        EL_IDS.overlay.classList.add('visible')
+    },
+
+    displayGameOverText(player) {
+        if(player.isWinner) {
+            gameOverText.innerText = `${player.playerName} is the winner`
+        } else if (state.gameMode === GAME_MODE.GAME_DRAW) {
+            gameOverText.innerText = `It's a draw`
+        }
+    }
 }
