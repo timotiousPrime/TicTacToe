6359a3a156217201336cdd7ad7555312d4d29987
timotiousPrime <timodb031@gmail.com>
2021-10-11 01:16:18 +0200

    sudo code

--- hardBot.js
+++ hardBot.js
@@ -2,8 +2,31 @@
 // Using mini max algo
 import { currentBoard } from "./newGameLogic.js"
 
+// function findBestMove(board):
+//     bestMove = NULL
+//     for each move in board :
+//         if current move is better than bestMove
+//             bestMove = current move
+//     return bestMove
 
+// function minimax(board, depth, isMaximizingPlayer):
 
+//     if current board state is a terminal state :
+//         return value of the board
+    
+//     if isMaximizingPlayer :
+//         bestVal = -INFINITY 
+//         for each move in board :
+//             value = minimax(board, depth+1, false)
+//             bestVal = max( bestVal, value) 
+//         return bestVal
+
+//     else :
+//         bestVal = +INFINITY 
+//         for each move in board :
+//             value = minimax(board, depth+1, true)
+//             bestVal = min( bestVal, value) 
+//         return bestVal 
 
 
 
