52a795de093d07e7e31395da197f9551d4918b67
timotiousPrime <timodb031@gmail.com>
2021-10-12 22:38:38 +0200

    psuedo code for minimax algo

--- hardBot.js
+++ hardBot.js
@@ -49,3 +49,89 @@ export function getEasyAiChoice(state){
     return cellValue
 }
 
+// check if there are moves remaining on the board
+function isMovesLeft(board){
+    // return true if there are 
+    // else return false if there are no moves left
+}
+
+function evaluate(board){
+    // check for X or O victory
+        // return +10 if there is a win for X
+        // else return -10 for O win
+
+    // if no wins then return 0
+}
+
+function minimax(board, depth, isMax) {
+    // gets the score of the current board
+    let score = evaluate(board)
+
+    // If the maximizer has won, 
+    // return their evaluated score
+    if (score === 10) return score
+    
+
+    // If the minimizer has won, 
+    // return their evaluated score
+    if (score === -10) return score
+    
+    // check if it's a tie
+    if (isMovesLeft(board) == false) return 0
+
+    if (isMax) {
+        let best = -1000
+
+        // check all cells 
+            // if cell is empty
+                // make a move in cell
+                // call minimax recursively
+                // and choose max value
+                best = Math.max(best, minimax(board, depth +1, !isMax))
+                // undo the move
+                return best
+    }
+
+    // if this minimizers turn
+    else {
+        let best = 1000
+
+        // check all cells 
+            // if cell is empty
+                // make a move in cell
+                // call minimax recursively
+                // and choose max value
+                best = Math.min(best, minimax(board, depth +1, !isMax))
+                // undo the move
+                return best
+    }
+}
+
+function findBestMove(board){
+    let bestVal = -1000
+    // let bestMove = new move
+    // bestMove = cell
+
+    // check all cells, evaluate 
+    // minimax function for all empty
+    // cells. Return the cell
+    // with optimal value
+    
+    // check all cells
+        // if cell is empty
+            // make a move in cell
+            // compute evaluate func for this move
+            let moveVal = minimax(board, 0, false)
+
+            // undo the move 
+
+            // If the value of the current move
+            // is more than the best value, then
+            // update best 
+            if (moveVal > bestVal){
+                bestMove = cell
+                bestVal = moveVal
+            }
+            return bestMove
+}
+
